5a3707bfa7ccb0ecc24e5ae62c6af831b8feee6a
Jhon <jhon@comeca.ai>
2025-05-09 19:21:58 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
-
-from deploy_dashboards_online import create_sample_data, create_app
+from deploy_dashboards_online import create_app, create_sample_data
 
 data = create_sample_data()
-
 app = create_app(data)
 server = app.server
